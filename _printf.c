431735bd5268c2ffec8a8b32db81b141343b406b
Omaraoo <omaraoo98@gmail.com>
2023-10-17 12:53:26 -0700

    Omar

--- _printf.c
+++ _printf.c
@@ -16,9 +16,9 @@ params_t params = PARAMS_INIT;
 
 va_start(ap, format);
 
-if (!format || (format[0] == ' % ' && !format[1]))
+if (!format || (format[0] == '%' && !format[1]))
 return (-1);
-if (format[0] == ' % ' && format[1] == ' ' && !format[2])
+if (format[0] == '%' && format[1] == ' ' && !format[2])
 return (-1);
 for (p = (char *)format; *p; p++)
 {
